5b28e7a1d8a641a78610e269fecb0eae8ebb5427
Matthias Diener <mdiener@illinois.edu>
2022-02-23 12:37:50 -0600

    remove patches, add tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+set -ex
+
 cmake ${CMAKE_ARGS} -D CMAKE_BUILD_TYPE=Release     \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_INSTALL_LIBDIR=lib     \
       -D BUILD_SHARED_LIBS=ON         \
       -D BUILD_EXAMPLES=OFF           \
-      -D BUILD_DOXYGEN=OFF            \
       $SRC_DIR
 
-make install -j${CPU_COUNT}
+make -j${CPU_COUNT}
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+    ctest -V
+fi
+
+make install
