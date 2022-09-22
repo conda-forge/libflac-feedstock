af8f7aaf392622c7d52dcc4be81347b50a12c810
Matthias Diener <matthias.diener@gmail.com>
2022-09-22 10:54:38 -0500

    see if we can build manpages

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,6 @@ cmake ${CMAKE_ARGS} -D CMAKE_BUILD_TYPE=Release     \
       -D CMAKE_INSTALL_LIBDIR=lib     \
       -D BUILD_SHARED_LIBS=ON         \
       -D BUILD_EXAMPLES=OFF           \
-      -D INSTALL_MANPAGES=off         \
       $SRC_DIR
 
 make -j${CPU_COUNT}
